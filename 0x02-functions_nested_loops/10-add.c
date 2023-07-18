2a7776f708353356ed698513670d976f73c46783
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-07-18 14:23:39 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*add - ADDtwo nums
+*
+*Description: 'add a to b and return '
+*@a: number a
+*@b: number b
+**Return: a+b
+*/
+int add(int a, int b)
+{
+	return (a + b);
+}
