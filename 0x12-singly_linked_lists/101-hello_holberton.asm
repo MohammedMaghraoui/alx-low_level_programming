c34403412cb5ed58f5312d5378a709dac9a1beb1
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-08-24 13:49:52 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,9 @@
-section .data
-    hello_msg db "Hello, Holberton", 10, 0  ; The message to print, followed by a new line and null terminator
-
-section .text
-    global main
-    extern printf
-
+global    main
+          extern    printf
 main:
-    ; Function Prologue
-    sub rsp, 8    ; Adjust the stack pointer for alignment
-
-    ; Call printf
-    mov rdi, hello_msg  ; Load the address of the message into rdi
-    call printf         ; Call the printf function
-
-    ; Function Epilogue
-    add rsp, 8    ; Restore the stack pointer
-    ret           ; Return from the program
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
