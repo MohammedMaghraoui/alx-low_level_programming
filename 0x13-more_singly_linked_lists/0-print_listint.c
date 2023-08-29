d7061053a4bcf9c7bce4309f3ce0066d3513a627
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-08-29 10:51:52 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -18,9 +18,9 @@ size_t print_listint(const listint_t *h)
 		if (h->n)
 			printf("%d\n", h->n);
 		root++;
-		h = h>next;
+		h = h->next;
 	}
-	if (h->new)
+	if (h->n)
 		printf("%d\n", h->n);
 	root++;
 	return (root);
