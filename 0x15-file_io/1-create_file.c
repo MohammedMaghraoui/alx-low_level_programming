d2c3a13e81ed134264238590c7d687e8ddece723
MohammedMaghraoui <mohamadmaghraoui99@gmail.com>
2023-09-04 14:56:03 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,55 +1,33 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <string.h>
+#include "main.h"
+
 /**
- * create_file- write content to a file , if it exists
- * , if not the function will create it and write to it
- * @filename: is the file name
- * @text_content: the content that the function will write to the file
- * Return: 0 if ok , -1 if an error raised
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-	int file;
-	int flag; /*0 if the file exists , 1 if the file has been created*/
-	int trunc_code;
-	int write_code;
-	unsigned int len;
-	mode_t FILE_PERMITIONS;
+	int fd, w, len = 0;
 
-	if (text_content == NULL)
-		return (1);
-	len = strlen(text_content);
-	/*read and write permission for the owner*/
-	FILE_PERMITIONS = S_IRUSR | S_IWUSR;
 	if (filename == NULL)
 		return (-1);
-	file = open(filename, O_RDWR, FILE_PERMITIONS);/*file already exists*/
-	flag = 0;
-	if (file == -1)
+
+	if (text_content != NULL)
 	{
-		file = open(filename, O_CREAT | O_RDWR, FILE_PERMITIONS);/*file exist*/
-		flag = 1;
-		if (file == -1)
-			return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	if (flag == 0)/*exists*/
-	{
-		trunc_code = ftruncate(file, len);
-		if (trunc_code != 0)
-			return (-1);
-		write_code = write(file, text_content, len);
-		if (write_code == -1)
-			return (-1);
-	}
-	else if (flag == 1)/* !exists*/
-	{
-		write_code = write(file, text_content, len);
-		if (write_code == -1)
-			return (-1);
-	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, len);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
 	return (1);
 }
